cd6b83d0c77c8459f7b58e483ed60ed1b365937b
Norman Nuthu <normannuthu@Normans-MacBook-Pro.local>
2020-01-14 05:18:27 +0300

    Initialized Repo

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Delayed scrolling to different sections of the page
+// Modified from a tutorial found at
+// https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+$('#contact-form').on('submit', function(e) {
+    $('#contactFormModal').modal('show');
+    e.preventDefault();
+})
